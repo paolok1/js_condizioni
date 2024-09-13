3ce2e48506f28c2935786574f5943b7ab4f4717e
paolok1 <gpaolok1@gmail.com>
2024-09-13 16:33:18 +0200

    selfwork_condizioni1

--- main.js
+++ main.js
@@ -1,17 +1,58 @@
-let voto  = 18;
-if (voto < 18 && voto >=0 ) {
- console.log("Insufficiente");
-}else if (voto < 21 && voto >= 18 ) {
-    console.log("Sufficiente");
-}else if (voto >21 && voto <= 24 ) {
-  console.log("Buono");
-}else if (voto >24 && voto <=27 ) {
-  console.log("Distinto");
-}else if (voto > 27 && voto <= 29 ) {
-    console.log("Ottimo");
-}else if (voto ==30 ) {
-    console.log("Eccellente");
+// let voto  = 18;
+// if (voto < 18 && voto >=0 ) {
+//  console.log("Insufficiente");
+// }else if (voto < 21 && voto >= 18 ) {
+//     console.log("Sufficiente");
+// }else if (voto >21 && voto <= 24 ) {
+//   console.log("Buono");
+// }else if (voto >24 && voto <=27 ) {
+//   console.log("Distinto");
+// }else if (voto > 27 && voto <= 29 ) {
+//     console.log("Ottimo");
+// }else if (voto ==30 ) {
+//     console.log("Eccellente");
 
-}else{
-    console.log("voto non valido");
-}  
+// }else{
+//     console.log("voto non valido");
+// }  
+
+
+
+let voto = 31;
+switch(voto){
+  case 18 :
+    console.log('Voto Sufficiente');
+    break;
+
+    case 24 :
+      console.log('voto Buono');
+      break;
+
+    case 27 :
+      console.log('Voto Distinto');
+      break;
+
+      case 29 :
+        console.log('Voto Ottimo');
+        break;
+
+        case 30: 
+        console.log('Voto Eccellente!');
+        break;
+
+        default :
+        console.log('Voto non valido');
+    }
+
+      
+      
+
+        
+        
+        
+
+
+      
+
+  
+    
