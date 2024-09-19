c646e29bad4aa74b974ce54f99e8ecfbf7160a7d
paolok1 <gpaolok1@gmail.com>
2024-09-19 16:53:49 +0200

    selfwork cicli

--- selfwork2.js
+++ selfwork2.js
@@ -13,32 +13,32 @@
 
 
 
-let temperatura = -10;
+// let temperatura = -10;
 
 
-switch (true) {
-    case temperatura >=-9 <0:
-       console.log("Non è tanto il freddo quanto l'umidità");
-       break;
+// switch (true) {
+//     case temperatura >=-9 <0:
+//        console.log("Non è tanto il freddo quanto l'umidità");
+//        break;
 
-       case temperatura <=-10:
-        console.log("Copriti...ancora ti raffreddi");
-        break;
+//        case temperatura <=-10:
+//         console.log("Copriti...ancora ti raffreddi");
+//         break;
 
-        case temperatura >=0 && temperatura <20:
-            console.log("Non ci sono più le mezze stagioni");
-            break;
+//         case temperatura >=0 && temperatura <20:
+//             console.log("Non ci sono più le mezze stagioni");
+//             break;
 
-            case temperatura >=20 && temperatura <30:
-                console.log("Mi dia una Peroni sudata");
-                break;
+//             case temperatura >=20 && temperatura <30:
+//                 console.log("Mi dia una Peroni sudata");
+//                 break;
 
-            case temperatura >=30:
-                console.log("Lu mare, lu sole, lu ientu");
-                break;
+//             case temperatura >=30:
+//                 console.log("Lu mare, lu sole, lu ientu");
+//                 break;
                 
-                default:
-                }
+//                 default:
+//                 }
         
         
 
