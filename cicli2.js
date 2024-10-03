f99b3faeb0f2152a54ebb5f4f6c7fe9043d3300e
paolok1 <gpaolok1@gmail.com>
2024-10-03 17:31:37 +0200

    selfwork cicli3

--- cicli2.js
+++ cicli2.js
@@ -1,20 +1,20 @@
-let numDispari = 0;
-let sommaDispari = 0;
+// let numDispari = 0;
+// let sommaDispari = 0;
 
-for(let i = 1; i <=20 ; i++){
-  if ( i % 2 ==0){
-    console.log(i);
-  } else{
-    numDispari = numDispari + i;
-    sommaDispari ++;
-  }
-}
+// for(let i = 1; i <=20 ; i++){
+//   if ( i % 2 ==0){
+//     console.log(i);
+//   } else{
+//     numDispari = numDispari + i;
+//     sommaDispari ++;
+//   }
+// }
 
-let mediaDispari = numDispari / sommaDispari;
-console.log( " i numeri dispari sono " + sommaDispari);
-console.log( " La somma dei numeri dispari è : " + numDispari);
+// let mediaDispari = numDispari / sommaDispari;
+// console.log( " i numeri dispari sono " + sommaDispari);
+// console.log( " La somma dei numeri dispari è : " + numDispari);
 
-console.log(`La media dei numeri dispari è : ${+ mediaDispari}`);
+// console.log(`La media dei numeri dispari è : ${+ mediaDispari}`);
 
   
   
---
+++ cicli3.js
@@ -0,0 +1,16 @@
+let selezione = prompt("Seleziona un numero per la bevanda da 1 a 3 \n 1 Acqua \n 2 Coca-cola \n 3 Birra");
+let numSelezione = 0;
+
+for(let i = 1; i <=3; i++ );
+if (selezione == 1) {
+    console.log("Hai selezionato Acqua");
+    
+} else if (selezione == 2){
+    console.log("Hai selezionato Coca-cola");
+} else if(selezione == 3){
+    console.log("Hai selezionato Birra");
+}  else if(selezione === 1, 2, 3 ){
+    prompt("Seleziona un numero per la bevanda da 1 a 3 \n 1 Acqua \n 2 Coca-cola \n 3 Birra");
+}
+ 
+    
