d303de82ed09bd9554e191e8554ff748ae589f86
paolok1 <gpaolok1@gmail.com>
2024-10-11 18:34:48 +0200

    selfwork funzioni2 corretto

--- selfwork_funzioni3.js
+++ selfwork_funzioni3.js
@@ -1,13 +1,13 @@
-function contaCifre(numero) {
-    if (numero > 9999) {
-        return 'Numero troppo grande';
-    } else {
-        return `${numero}  ${numero.toString().length} cifra${numero.toString().length > 1 ? '/e' : '' }`;
-    }
-}
+// function contaCifre(numero) {
+//     if (numero > 9999) {
+//         return 'Numero troppo grande';
+//     } else {
+//         return `${numero}  ${numero.toString().length} cifra${numero.toString().length > 1 ? '/e' : '' }`;
+//     }
+// }
 
-console.log(contaCifre(9));    
-console.log(contaCifre(99)); 
-console.log(contaCifre(999));
-console.log(contaCifre(9999));   
-console.log(contaCifre(12000));
+// console.log(contaCifre(9));    
+// console.log(contaCifre(99)); 
+// console.log(contaCifre(999));
+// console.log(contaCifre(9999));   
+// console.log(contaCifre(12000));
