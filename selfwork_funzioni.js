35e33b5c338aaad2dc7b2e2484e6a1e2db1c7b53
paolok1 <gpaolok1@gmail.com>
2024-10-11 14:26:26 +0200

    Selfwork funzioni2

--- selfwork_funzioni.js
+++ selfwork_funzioni.js
@@ -1,38 +1,38 @@
 // Non riesco a far stampare il risultato finale con il verdetto di vittoria
 
 
-function playGames(tiri) {
-   let player1 = 0;
-   let player2 = 0;
+// function playGames(tiri) {
+//    let player1 = 0;
+//    let player2 = 0;
    
-   for (let i = 0; i <= tiri ; i++) {
-    let player1Rules = Math.floor(Math.random() * 6 ) + 1;
+//    for (let i = 0; i <= tiri ; i++) {
+//     let player1Rules = Math.floor(Math.random() * 6 ) + 1;
     
-    console.log(`Al tiro ${i} il giocatore 1 ha ottenuto il punteggio di ${player1}`);
+//     console.log(`Al tiro ${i} il giocatore 1 ha ottenuto il punteggio di ${player1}`);
     
-    let player2Rules = Math.floor(Math.random() * 6 ) + 1;
+//     let player2Rules = Math.floor(Math.random() * 6 ) + 1;
 
-    console.log(`Al tiro ${i} il giocatore 2 ha ottenuto il punteggio di ${player2}`);
+//     console.log(`Al tiro ${i} il giocatore 2 ha ottenuto il punteggio di ${player2}`);
 
-    player1 = player1 + player1Rules;
-    player2 = player2 + player2Rules;
-   }
+//     player1 = player1 + player1Rules;
+//     player2 = player2 + player2Rules;
+//    }
    
-   if (player1 > player2) {
-    return `Il giocatore 1 ha vinto con un punteggio pari a ${player1} `
-   }else if (player2 > player1){
-    return `Il giocatore 2 ha vinto con un punteggio pari a ${player2}`
-   }else{
-    return `I giocatori hanno pareggiato con un punteggio pari a ${player1}`
-   }   
+//    if (player1 > player2) {
+//     return `Il giocatore 1 ha vinto con un punteggio pari a ${player1} `
+//    }else if (player2 > player1){
+//     return `Il giocatore 2 ha vinto con un punteggio pari a ${player2}`
+//    }else{
+//     return `I giocatori hanno pareggiato con un punteggio pari a ${player1}`
+//    }   
 
-}
+// }
         
- let tiri = Number(prompt ("Quanti tiri devono effettuare i giocatori?"));      
+//  let tiri = Number(prompt ("Quanti tiri devono effettuare i giocatori?"));      
         
- let result = playGames(tiri);       
+//  let result = playGames(tiri);       
         
- console.log(result);
+//  console.log(result);
   
         
     
---
+++ selfworkfunzioni2.js
@@ -0,0 +1,24 @@
+function conta(n) {
+
+
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0) {
+            console.log(`Il numero ${i} è sostituito dalla parola Fizz`);
+        }else if (i % 5 === 0) {
+            console.log(`Il numero ${i} è sostituito dalla parola Buzz`);
+        }else if (i % 15 === 0){
+            console.log(`Il numero ${i} è sostituito dalla parola FizzBuzz`);
+            
+        }else {
+            console.log("i");
+            
+        }
+
+    }
+            
+}
+            
+ conta(50);           
+        
+    
