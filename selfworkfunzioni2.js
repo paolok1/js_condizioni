1a4363d7189c09357598fc345c5025ef6c31e8b3
paolok1 <gpaolok1@gmail.com>
2024-10-12 10:46:14 +0200

    selfwork funzioni2 modificato

--- selfworkfunzioni2.js
+++ selfworkfunzioni2.js
@@ -1,24 +1,24 @@
-function conta(n) {
+// function conta(n) {
 
 
 
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0) {
-            console.log(`Il numero ${i} è sostituito dalla parola Fizz`);
-        }else if (i % 5 === 0) {
-            console.log(`Il numero ${i} è sostituito dalla parola Buzz`);
-        }else if (i % 15 === 0){
-            console.log(`Il numero ${i} è sostituito dalla parola FizzBuzz`);
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 3 === 0) {
+//             console.log(`Il numero ${i} è sostituito dalla parola Fizz`);
+//         }else if (i % 5 === 0) {
+//             console.log(`Il numero ${i} è sostituito dalla parola Buzz`);
+//         }else if (i % 8 === 0){
+//             console.log(`Il numero ${i} è sostituito dalla parola FizzBuzz`);
             
-        }else {
-            console.log(i);
+//         }else {
+//             console.log(i);
             
-        }
+//         }
 
-    }
+//     }
             
-}
+// }
             
- conta(50);           
+//  conta(50);           
         
     
