876a4362a223f504b10fb3b225b31d616313eeea
paolok1 <gpaolok1@gmail.com>
2024-10-14 13:15:58 +0200

    selfwork_funzioni4 modificato

--- selfwork_funzioni4.js
+++ selfwork_funzioni4.js
@@ -1,12 +1,29 @@
 
- let str = "i topi non avevano nipoti"
-let splitted =str.replace(/\W/g, "") ;
-console.log(splitted);
+//  let str = "i topi non avevano nipoti"
+// let splitted =str.replace(/\W/g, "") ;
+// console.log(splitted);
 
-if (str == splitted) {
-    console.log(`True`);
+// if (str == splitted) {
+//     console.log(`True`);
     
-}else{
-console.log(`False`);
+// }else{
+// console.log(`False`);
+// }
+
+
+function controlString(stringa) {
+    stringa = stringa.replace(/\W/g, "");
+
+    let reversed = stringa.split(``).reverse().join(``);
+    
+    
+    if (stringa == reversed) {
+        return true;
+        
+    } else {
+       return false; 
+    }
 }
 
+console.log(controlString("i topi non avevano nipoti"));
+ 
