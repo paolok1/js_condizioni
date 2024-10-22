3f23c4c3c41b92c680fbe94a5388a2bacedaa365
paolok1 <gpaolok1@gmail.com>
2024-10-22 17:27:46 +0200

    Selfwork1 oggetti

---
+++ selfworkOggetti.js
@@ -0,0 +1,11 @@
+let presentazione = {
+'nome' : 'Paolo',
+'cognome' : 'Giuliani',
+'età' : 47,
+'saluta' : function() {
+    // console.log(`Salve a tutti, mi chiamo Paolo Giuliani ed ho 47 anni `);  
+}
+};
+
+console.log('Salve a tutti mi chiamo', presentazione.nome , presentazione.cognome, 'ed ho', presentazione.età, 'anni');
+;
--- selfwork_array2.js
+++ selfwork_array2.js
@@ -1,16 +1,16 @@
 
     
     
-    let array  = [3, 5, 10, 2, 8];
-    let somma = array.reduce((acc, n)=> acc + n);
-    console.log(somma);
+    // let array  = [3, 5, 10, 2, 8];
+    // let somma = array.reduce((acc, n)=> acc + n);
+    // console.log(somma);
     
-    let media = somma / array.length;
-    console.log(media);
+    // let media = somma / array.length;
+    // console.log(media);
     
-    let valoriMinori = array.filter((numero)=> numero < media);
-    console.log(valoriMinori);
-    console.log(`La media è = ${media}, i valori minori sono = ${valoriMinori}`);
+    // let valoriMinori = array.filter((numero)=> numero < media);
+    // console.log(valoriMinori);
+    // console.log(`La media è = ${media}, i valori minori sono = ${valoriMinori}`);
     
     
     
